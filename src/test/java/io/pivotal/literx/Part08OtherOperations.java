61d3efb5ada65572f35eb6dbf52c2331da62e593
Bogdan Calmac <bcalmac@gmail.com>
2017-03-19 22:57:56 -0700

    Solve Part08OtherOperations

--- src/test/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/test/java/io/pivotal/literx/Part08OtherOperations.java
@@ -31,9 +31,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO Create a Flux of user from Flux of username, firstname and lastname.
+	// DONE Create a Flux of user from Flux of username, firstname and lastname.
 	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux).map(t -> new User(t.getT1(), t.getT2(), t.getT3()));
 	}
 
 //========================================================================================
@@ -55,9 +55,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO return the mono which returns faster its value
+	// DONE return the mono which returns faster its value
 	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return Mono.first(mono1, mono2);
 	}
 
 //========================================================================================
@@ -79,9 +79,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO return the flux which returns faster the first value
+	// DONE return the flux which returns faster the first value
 	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		return Flux.firstEmitting(flux1, flux2);
 	}
 
 //========================================================================================
@@ -94,9 +94,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+	// DONE Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
 	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
+		return flux.then();
 	}
 
 //========================================================================================
@@ -112,9 +112,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams does not accept null values)
+	// DONE Return a valid Mono of user for null input and non null input user (hint: Reactive Streams does not accept null values)
 	Mono<User> nullAwareUserToMono(User user) {
-		return null;
+		return Mono.justOrEmpty(user);
 	}
 
 //========================================================================================
@@ -131,9 +131,9 @@ public class Part08OtherOperations {
 				.verifyComplete();
 	}
 
-	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+	// DONE Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
 	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
+		return mono.defaultIfEmpty(User.SKYLER);
 	}
 
 }
